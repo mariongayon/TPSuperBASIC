1e67137424f9f0cc8e04442631572fd479ef40bb
Marion Gayon <mariongayon@gmail.com>
2022-06-22 11:50:34 +0200

    adjust Remtest

--- TestsSuperBASIC/RemTest.cs
+++ TestsSuperBASIC/RemTest.cs
@@ -15,14 +15,14 @@ namespace TestsSuperBASIC
             Library lib = new Library();
 
             lib.AddFunction(new SuperBASIC.Functions.Rem(), 2, "REM");
-            lib.AddFunction(print, 2, "PRINT");
+            lib.AddFunction(print, 1, "PRINT");
 
 
             Runtime r = new Runtime(lib);
             r.OpenFile(Directory.GetCurrentDirectory() + "\\CasDeTest\\RemTest1.txt");
             r.Run();
 
-            Assert.AreEqual((float)print.output[2], 2);
+            Assert.AreEqual((float)print.output[0], 0);
         }
     }
 }
