3f44907d54b1c4e401ec957a7b7f6eab886ce401
Mathieu Cadi <mathieuc001@hotmail.com>
2022-06-22 21:25:11 +0200

    appdomain add

---
+++ SuperBASIC/AppDomainBase.cs
@@ -0,0 +1,39 @@
+﻿namespace SuperBASIC
+{
+    internal class AppDomainBase
+    {
+        private static void AppDomain(string[] args)
+        {
+            try
+            {
+                Library lib = new Library();
+#if MEMORY
+                lib.AddFunction(new Functions.MemoryLoad(), 1, "MEMLOAD");
+                lib.AddFunction(new Functions.MemoryStore(), 2, "MEMSTORE");
+#endif
+                lib.AddFunction(new Functions.Print(), 1, "PRINT");
+                lib.AddFunction(new Functions.Multiply(), 2, "MULTIPLY");
+                lib.AddFunction(new Functions.Add(), 2, "ADD");
+                lib.AddFunction(new Functions.Compare(), 2, "COMPARE");
+                lib.AddFunction(new Functions.JumpZero(), 2, "JZ");
+                lib.AddFunction(new Functions.Goto(), 1, "GOTO");
+                lib.AddFunction(new Functions.Pi(), 0, "PI");
+                lib.AddFunction(new Functions.Euler(), 0, "EULER");
+                Runtime r = new Runtime(lib);
+                if (args.Length <= 1)
+                {
+                    r.OpenFile(Directory.GetCurrentDirectory() + "\\Test.basic");
+                }
+                else
+                {
+                    r.OpenFile(args[1]);
+                }
+                r.Run();
+            }
+            catch (Parser.ParseException e)
+            {
+                Console.WriteLine($"Parsing failed:\n{e}");
+            }
+        }
+    }
+}
